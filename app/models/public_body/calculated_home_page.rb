152b0849e2f2a05bc422f076a916e3e1bb7d01cc
Gareth Rees <gareth@garethrees.co.uk>
2023-07-24 10:06:13 +0100

    Exclude email providers from body home pages
    Many smaller authorities use a request email address from a general
    purpose public email provider. We don't want e.g. gmail.com being set as
    the home page for the authority.
    
    This is configurable in the theme by setting the class attribute:
    
        # Add to the defaults
        PublicBody.excluded_calculated_home_page_domains << %w[
          example.net
        ]
    
        # Clear the defaults and set your own list
        PublicBody.excluded_calculated_home_page_domains = %w[
          example.org
          example.net
        ]
    
    Fixes https://github.com/mysociety/alaveteli/issues/6434

--- app/models/public_body/calculated_home_page.rb
+++ app/models/public_body/calculated_home_page.rb
@@ -1,5 +1,27 @@
 # Guess the home page based on the request email domain.
 module PublicBody::CalculatedHomePage
+  extend ActiveSupport::Concern
+
+  included do
+    cattr_accessor :excluded_calculated_home_page_domains, default: %w[
+      aol.com
+      btinternet.com
+      gmail.com
+      gmx.com
+      hotmail.com
+      icloud.com
+      mail.com
+      mail.ru
+      outlook.com
+      protonmail.com
+      qq.com
+      yahoo.com
+      yandex.com
+      ymail.com
+      zoho.com
+    ]
+  end
+
   # Guess home page from the request email, or use explicit override, or nil
   # if not known.
   #
@@ -9,7 +31,14 @@ module PublicBody::CalculatedHomePage
     if home_page && !home_page.empty?
       home_page[URI.regexp(%w(http https))] ? home_page : "https://#{home_page}"
     elsif request_email_domain
+      return if excluded_calculated_home_page_domain?(request_email_domain)
       "https://www.#{request_email_domain}"
     end
+  end
+
+  private
+
+  def excluded_calculated_home_page_domain?(domain)
+    excluded_calculated_home_page_domains.include?(domain)
   end
 end
